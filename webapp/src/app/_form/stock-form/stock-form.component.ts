52a65668ee888b17db349d606a40b6d698b1b2a3
golesny <golesny@users.noreply.github.com>
2019-06-30 21:02:07 +0200

    sympol suggestion on stock form

--- webapp/src/app/_form/stock-form/stock-form.component.ts
+++ webapp/src/app/_form/stock-form/stock-form.component.ts
@@ -14,6 +14,8 @@ export class StockFormComponent {
   currencies:Map<string, number>;
   public model:ISIN;
   public mode:string;
+  symbolproposals: Map<string, string>;
+  symbolproposalstatus: string;
 
   constructor(private dataService:DataService,
               private notifyService: NotifyService,
@@ -28,11 +30,15 @@ export class StockFormComponent {
   resetISIN() {
     this.model = new ISIN('', '', "EUR", "", "");
     this.mode = "new";
+    this.symbolproposals = new Map<string, string>();
+    this.symbolproposalstatus = "oi-magnifying-glass";
   }
 
-  editISIN(isin) {
+  editISIN(isin, $formEl) {
+    this.symbolproposals = new Map<string, string>();
     this.model = JSON.parse(JSON.stringify(isin)); // use a copy
     this.mode = "edit";
+    $formEl.scrollIntoView({block: "start", inline: "nearest"});
   }
 
   loadISINList() : void {
@@ -58,6 +64,26 @@ export class StockFormComponent {
       console.error('ERROR: ${error.message}');
       this.notifyService.showError("error ", error.message);
     }
+    );
+  }
+
+  getAlphaVantageSymbol() {
+    var keyword = this.model.symbol;
+    if (keyword.length == 0) {
+      keyword = this.model.name;
+    }
+    this.symbolproposalstatus = "oi-cloud-download";
+    this.dataService.getAlphaVantageProposals(keyword).subscribe(
+      data => {
+        this.symbolproposalstatus = "oi-magnifying-glass";
+        data["bestMatches"].forEach(proposal => {
+          this.symbolproposals.set(proposal["1. symbol"], proposal["1. symbol"] + " | " + proposal["2. name"] + " (" + proposal["3. type"] + "/" + proposal["8. currency"]+")");
+        });
+      },
+      err => {
+        this.notifyService.showError("error on loading alpha vantage proposals", err);
+        this.symbolproposalstatus = "oi-warning";
+      }
     );
   }
 
